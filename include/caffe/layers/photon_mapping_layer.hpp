d65e5569829e3bd2f0f00cd365b04df2270755fa
bactlink <bactlink@gmail.com>
2018-02-02 21:58:32 +0800

    Add photon mapping layer

---
+++ include/caffe/layers/photon_mapping_layer.hpp
@@ -0,0 +1,48 @@
+#ifndef CAFFE_PHOTONMAPPING_LAYER_HPP_
+#define CAFFE_PHOTONMAPPING_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+template <typename Dtype>
+class PhotonMappingLayer : public Layer<Dtype> {
+ public:
+  explicit PhotonMappingLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "PhotonMapping"; }
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down,
+      const vector<Blob<Dtype>*>& bottom) {};
+
+  Blob<Dtype> mean_, variance_, temp_, x_norm_;
+  bool use_global_stats_;
+  Dtype moving_average_fraction_;
+  int channels_;
+  Dtype eps_;
+
+  // extra temporarary variables is used to carry out sums/broadcasting
+  // using BLAS
+  Blob<Dtype> batch_sum_multiplier_;
+  Blob<Dtype> num_by_chans_;
+  Blob<Dtype> spatial_sum_multiplier_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_PHOTONMAPPING_LAYER_HPP_
---
+++ src/caffe/layers/photon_mapping_layer.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <vector>
+
+#include "caffe/layers/photon_mapping_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void PhotonMappingLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+}
+
+template <typename Dtype>
+void PhotonMappingLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  vector<int> new_shape = bottom[0]->shape();
+  CHECK_EQ(new_shape.size(), 4);
+  new_shape[1] = 1;
+  top[0]->Reshape(new_shape);
+}
+
+template <typename Dtype>
+void PhotonMappingLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  Dtype* top_data = top[0]->mutable_cpu_data();
+  const int num = bottom[0]->shape(0);
+  const int photon_num = bottom[0]->shape(1) / 5;
+  const int H = bottom[0]->shape(2);
+  const int W = bottom[0]->shape(3);
+
+  for (int n = 0; n < num; ++n) {
+    for (int i = 0; i < H; ++i)
+      for (int j = 0; j < W; ++j) {
+        double max_squ_dis = 0;
+        double flux = 0;
+        const int index = i * W + j;
+        for (int p = 0; p < photon_num; ++p) {
+          const Dtype* squ_dis_data = bottom_data + (p * 5) * H * W;
+          const Dtype* flux_data = bottom_data + (p * 5 + 1) * H * W;
+          max_squ_dis = std::max(max_squ_dis,
+              static_cast<double>(squ_dis_data[index]));
+          flux += static_cast<double>(flux_data[index]);
+        }
+        top_data[index] = max_squ_dis > 1e-6 ? flux / max_squ_dis : 0;
+      }
+  }
+}
+
+INSTANTIATE_CLASS(PhotonMappingLayer);
+REGISTER_LAYER_CLASS(PhotonMapping);
+}  // namespace caffe
