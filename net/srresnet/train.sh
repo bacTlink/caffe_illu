94d707f52f26b460d1048f813d73aa00578616a5
bactlink <bactlink@gmail.com>
2018-04-05 14:32:48 +0800

    Update srresnet

--- net/srresnet/train.sh
+++ net/srresnet/train.sh
@@ -3,4 +3,4 @@ set -e
 
 CBIN=/home/linzehui/illu/build/caffe_illu/tools/caffe
 
-$CBIN train --solver=./solver.prototxt --log_dir log/ $@ -gpu 0,2 -weights snapshots/SRResNet-MSE_iter_70000.caffemodel
+$CBIN train --solver=./solver.prototxt --log_dir log/ $@ -gpu 0,2
