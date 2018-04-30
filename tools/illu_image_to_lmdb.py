0db7a39401382138f35ffe81099312eb102438aa
bactlink <bactlink@gmail.com>
2018-04-30 15:45:02 +0800

    Divide into train and test

--- tools/illu_image_to_lmdb.py
+++ tools/illu_image_to_lmdb.py
@@ -10,11 +10,27 @@ import lmdb
 import shutil
 import random
 
-check_mse = False
-src_dir = '/data3/lzh/100x224x224_ring_diff/'
-dst_dir = '/data3/lzh/100x10x224x224_ring_diff/'
+src_dir = '/data3/lzh/10000x672x672_box_diff/'
+dst_dir = '/data3/lzh/10000x10x224x224_box_diff/'
 filelist = os.path.join(src_dir, 'filelist.txt')
 img_count = 10
+
+if not os.path.exists(dst_dir):
+    os.makedirs(dst_dir)
+
+def sec_into_224(pics):
+    assert pics.ndim == 3
+    if pics.shape[1] == 224:
+        return pics.reshape(1, pics.shape[0], pics.shape[1], pics.shape[2])
+    assert pics.shape[1] == 672
+    for i in xrange(3):
+        for j in xrange(3):
+            tmp = pics[:, i*224:i*224+224, j*224:j*224+224].reshape(1, pics.shape[0], 224, 224)
+            if i == 0 and j == 0:
+                res = tmp
+            else:
+                res = np.append(res, tmp, axis = 0)
+    return res
 
 def mse(pic1, pic2):
     assert pic1.ndim == 2
@@ -31,46 +47,75 @@ def large_mse(label, data):
     return mse_res > 20
 
 count = 0
-path = os.path.join(dst_dir, 'label,data')
-shutil.rmtree(path, ignore_errors = True)
-if not os.path.exists(path):
-    os.makedirs(path)
-env = lmdb.open(path, map_size=int(1e12))
+train_path = os.path.join(dst_dir, 'train-label,data')
+test_path = os.path.join(dst_dir, 'test-label,data')
+shutil.rmtree(train_path, ignore_errors = True)
+shutil.rmtree(test_path, ignore_errors = True)
+train_env = lmdb.open(train_path, map_size=int(1e12))
+test_env = lmdb.open(test_path, map_size=int(1e12))
 
-with env.begin(write=True) as txn:
-    for line in open(filelist):
-        label_filename = line[:-1]
-        print label_filename
+train_txn = train_env.begin(write=True)
+test_txn = test_env.begin(write=True)
+cnt = 0
+for line in open(filelist):
+    label_filename = line[:-1]
+    print label_filename
 
-        # process label
-        label_img = caffe.io.load_image(os.path.join(src_dir, label_filename))
-        shape = label_img.shape
-        label = []
+    # process label
+    label_img = caffe.io.load_image(os.path.join(src_dir, label_filename))
+    shape = label_img.shape
+    label = []
+    for i in xrange(3):
+        label.append(label_img[:, :, i].reshape(1, shape[0], shape[1]))
+
+    # process data
+    base_filename = label_filename[:-9]
+    imgs = []
+    for i in xrange(1, img_count + 1):
+        filename = base_filename + '_' + str(i) + '.png'
+        imgs.append(caffe.io.load_image(os.path.join(src_dir, filename)))
+    data = []
+    for img in imgs:
+        assert (shape == img.shape)
         for i in xrange(3):
-            label.append(label_img[:, :, i].reshape(1, shape[0], shape[1]))
+            tmp_data = img[:, :, i].reshape(1, shape[0], shape[1])
+            if len(data) < 3:
+                data.append(tmp_data)
+            else:
+                data[i] = np.append(data[i], tmp_data, axis = 0)
 
-        # process data
-        base_filename = label_filename[:-9]
-        imgs = []
-        for i in xrange(1, img_count + 1):
-            filename = base_filename + '_' + str(i) + '.png'
-            imgs.append(caffe.io.load_image(os.path.join(src_dir, filename)))
-        data = []
-        for img in imgs:
-            assert (shape == img.shape)
-            for i in xrange(3):
-                tmp_data = img[:, :, i].reshape(1, shape[0], shape[1])
-                if len(data) < 3:
-                    data.append(tmp_data)
-                else:
-                    data[i] = np.append(data[i], tmp_data, axis = 0)
+    for i in xrange(3):
+        label[i] = sec_into_224(label[i])
+        data[i] = sec_into_224(data[i])
 
-        for i in xrange(3):
-            if (not check_mse) or large_mse(label[i], data[i]):
-                datum = caffe.io.array_to_datum(np.append(label[i], data[i], axis = 0))
-                if check_mse:
-                    img_id = str(random.randint(0, int(1e10))) + '-' + base_filename + 'c' + str(i)
-                else:
-                    img_id = base_filename + 'c' + str(i)
-                txn.put(img_id, datum.SerializeToString())
+    for i in xrange(3):
+        for j in xrange(data[i].shape[0]):
+            datum = caffe.io.array_to_datum(np.append(label[i][j,:,:,:], data[i][j,:,:,:], axis = 0))
+            if random.randint(0, 100) == 0:
+                classification = 'test'
+            else:
+                classification = 'train'
+            if classification == 'train':
+                img_id = str(random.randint(0, int(1e10))) + '-' + base_filename + 'c' + str(i)
+                train_txn.put(img_id, datum.SerializeToString())
+            else:
+                img_id = base_filename + 'c' + str(i)
+                test_txn.put(img_id, datum.SerializeToString())
+
+    cnt = cnt + 1
+    if (cnt == 100):
+        train_txn.commit()
+        test_txn.commit()
+        cnt = 0
+        train_txn = train_env.begin(write=True)
+        test_txn = test_env.begin(write=True)
+
+if cnt != 0:
+    train_txn.commit()
+    test_txn.commit()
+    cnt = 0
+    train_txn = train_env.begin(write=True)
+    test_txn = test_env.begin(write=True)
+train_env.close()
+test_env.close()
 
